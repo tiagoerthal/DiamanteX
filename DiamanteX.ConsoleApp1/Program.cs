11f191d2d80e172431e4d28c28b0c37d26f5d3bf
tiago <tiagoeweber@gmail.com>
2025-04-08 21:26:36 -0300

    implementação de erro caso utilize número 1

--- DiamanteX.ConsoleApp1/Program.cs
+++ DiamanteX.ConsoleApp1/Program.cs
@@ -4,17 +4,33 @@
     {
         static void Main(string[] args)
         {
-            ExibirCabecalho();
+            while (true)
+            {
+
+                ExibirCabecalho();
+
+                int numeroDoUsuario = Convert.ToInt32(Console.ReadLine());
+
+
+                if (numeroDoUsuario < 3 || numeroDoUsuario % 2 == 0)
+                {
+                    Console.WriteLine("Digite outro número, ímpar maior que 1");
+                   
+                }
+
+                Console.ReadLine();
+            }
+
            
         }
 
         static void ExibirCabecalho()
         {
+            Console.Clear();
             Console.WriteLine("---------------------------------------------");
             Console.WriteLine("Faça seu diamante!");
             Console.WriteLine("---------------------------------------------");
-            Console.Write("Digite um número inteiro para montar seu diamante: ");
-            Console.ReadLine();
+            Console.Write("Digite um número inteiro para montar seu diamante: ");       
         }
        
     }
