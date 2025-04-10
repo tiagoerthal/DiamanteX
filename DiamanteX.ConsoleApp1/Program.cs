efd86ca860fcc80df7ba048c84fb760eedd31308
tiago <tiagoeweber@gmail.com>
2025-04-09 21:28:24 -0300

    implementado o código que cria o diamante X

--- DiamanteX.ConsoleApp1/Program.cs
+++ DiamanteX.ConsoleApp1/Program.cs
@@ -1,23 +1,67 @@
-﻿namespace DiamanteX.ConsoleApp1
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace DiamanteX.ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            while (true)
+           
+            ExibirCabecalho();
+
+            int tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
+
+            ExibirMensagemDeErro(tamanhoDiamante);
+
+            Console.WriteLine("Digite ENTER para continuar....");
+              
+            int quantidadeLinhas =(tamanhoDiamante -  1) / 2;
+            int quantidadeEspaco = quantidadeLinhas;
+            int quantidadeX = 1;
+
+            // parte de cima
+            for (int i = 0; i < quantidadeLinhas; i++)
             {
+                for (int espaco = 0; espaco < quantidadeEspaco; espaco++)
+                    Console.Write(" ");
 
-                ExibirCabecalho();
+                for(int x = 0; x < quantidadeX; x++)
+                    Console.Write("X");
 
-                int numeroDoUsuario = Convert.ToInt32(Console.ReadLine());
-
-                ExibirMensagemDeErro(numeroDoUsuario);
-
-                Console.WriteLine("Digite ENTER para continuar....");
-                Console.ReadLine();
+                quantidadeX += 2;
+                quantidadeEspaco--;
+                
+                Console.WriteLine();
             }
 
-           
+            //parte do meio
+            for (int i = 0; i < tamanhoDiamante; i++)
+             Console.Write("X");
+
+                Console.WriteLine();
+
+
+
+            // parte de baixo
+            for (int i = 0; i < quantidadeLinhas; i++)
+            {
+                quantidadeX -= 2;
+                quantidadeEspaco++;
+
+                for (int espacos = 0; espacos < quantidadeEspaco; espacos++)
+                    Console.Write(" ");
+
+                for (int x = 0; x < quantidadeX; x++)
+                    Console.Write("X");
+
+               
+               
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+                                 
         }
 
         static void ExibirCabecalho()
@@ -29,10 +73,10 @@
             Console.Write("Digite um número inteiro para montar seu diamante: ");       
         }
        
-        static void ExibirMensagemDeErro(int numeroDoUsuario)
+        static void ExibirMensagemDeErro(int tamanhoDiamante)
         {
 
-            if (numeroDoUsuario < 3 || numeroDoUsuario % 2 == 0)
+            if (tamanhoDiamante < 3 || tamanhoDiamante % 2 == 0)
             {
                 Console.WriteLine("Digite outro número, ímpar maior que 1");
 
